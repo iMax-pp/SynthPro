07045ea1c353eabf1f7589afcecc504539557f4b
Julien Nevo <contact@julien-nevo.com>
2011-02-03 17:59:13 +0100

    WaveGenerator done, and a first Wave Generator (Dummy), as a quick test with the modules.

---
+++ src/main/abstraction/wavegenerator.h
@@ -0,0 +1,17 @@
+#ifndef WAVEGENERATOR_H
+#define WAVEGENERATOR_H
+
+/**
+  * Pure virtual class for the VCO to generate data from
+  * an input buffer to an output buffer.
+  */
+class WaveGenerator
+{
+public:
+    WaveGenerator() {}
+
+    virtual void generate(const float* bufferIn, float* bufferOut, int bufferLength, float frequency) = 0;
+};
+
+
+#endif // WAVEGENERATOR_H
