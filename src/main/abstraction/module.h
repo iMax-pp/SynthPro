81b1718b09b663d39dc3cbbfe9e71fbd375c6392
Julien Richard-Foy <julien@richard-foy.fr>
2011-02-03 22:30:25 +0100

    Add some comments to Module class

--- src/main/abstraction/module.h
+++ src/main/abstraction/module.h
@@ -6,13 +6,28 @@
 class In;
 class Out;
 
+/**
+ * A module is a component performing a single function (generating a signal, filtering, etc.)
+ */
 class Module : public QObject {
-public:
     Q_OBJECT
+
+public:
     Module(QObject* parent = 0);
+
+    /// Get the list of output ports of this module
     virtual QList<Out*>::const_iterator outports() const;
+
+    /// Get the list of input ports of this module FIXME is it needed?
     virtual QList<In*>::const_iterator inports() const;
+
+    /**
+     * Operation called by the sequencer. Subclasses have to implement their own.
+     */
     virtual void process() = 0;
+
+    /// Get the list of required modules for this module to perform its function
+    virtual QList<Module*>::const_iterator getReguirements() const = 0;
 
 private:
     QList<Out*> m_outports;
