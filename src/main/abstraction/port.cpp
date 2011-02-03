9f64db908fe86f83408b29f1f1e230033e879bc4
Julien Richard-Foy <10003735@e008m10.istic.univ-rennes1.fr>
2011-02-03 17:55:45 +0100

    Initial implementation of Port

--- src/main/abstraction/port.cpp
+++ src/main/abstraction/port.cpp
@@ -1,5 +1,27 @@
 #include "port.h"
 
-Port::Port()
+Port::Port(QObject* parent, bool replicable, bool gate) : QObject(parent), m_replicable(replicable), m_gate(gate), m_connection(0)
 {
+
+}
+
+Port::~Port() {}
+
+void Port::connectTo(Port* to)
+{
+    if (m_connection != to) {
+        m_connection = to;
+        to->connectTo(this);
+        emit connectedChanged(true); // FIXME This signal will be emitted 2 times, one time for each side of the association
+    }
+}
+
+bool Port::connected() const
+{
+    return m_connection != 0;
+}
+
+bool Port::isConnectable(const Port* to) const
+{
+    return (!connected() && to->out() != out() && to->gate() == gate());
 }
