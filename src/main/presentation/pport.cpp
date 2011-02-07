41eba93160378f3503c33dbac4398f0584292c0a
Maxime Simon <simon.maxime@gmail.com>
2011-02-07 23:58:57 +0100

    Increase PPort width (8 -> 10); Add shadow to PModule (thanks to QGraphicsDropShadowEffect).

--- src/main/presentation/pport.cpp
+++ src/main/presentation/pport.cpp
@@ -7,7 +7,7 @@ PPort::PPort(QGraphicsItem* parent)
     : QGraphicsEllipseItem(parent)
 {
     qDebug(QString("PPort::PPort parent = %1").arg((long)parent).toAscii());
-    setRect(0, 0, 8, 8);
+    setRect(0, 0, 10, 10);
     setBrush(QBrush(Qt::darkGreen));
     setPen(QPen(Qt::NoPen));
 }
