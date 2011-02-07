240b3cc3a404fe1bb35708c3de798330fcd1a022
Maxime Simon <simon.maxime@gmail.com>
2011-02-07 18:47:50 +0100

    Include QSlider in PVCO.

--- src/main/presentation/pvco.cpp
+++ src/main/presentation/pvco.cpp
@@ -5,10 +5,18 @@
 
 PVCO::PVCO()
 {
-    /* FIXME
+    // Resize PVCO to integrate the QSlider nicely.
+    setRect(0, 0, 200, 150);
+
+    // Create a QSlider.
     QSlider* waveSelector = new QSlider(Qt::Horizontal);
     waveSelector->setTickPosition(QSlider::TicksBelow);
     waveSelector->setRange(0, 3);
-    m_waveSelector = scene()->addWidget(waveSelector);
-    */
+
+    // Add it to the scene using a QGraphicsProxyWidget.
+    m_waveSelector = new QGraphicsProxyWidget(this);
+    m_waveSelector->setWidget(waveSelector);
+    // Static positioning, that ain't nice.
+    m_waveSelector->setPos(1, 120);
+    m_waveSelector->setMaximumWidth(199);
 }
