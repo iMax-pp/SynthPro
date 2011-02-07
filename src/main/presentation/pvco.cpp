4940b5fb66c743abddfd1dd42db67f41ad7dc755
Maxime Simon <simon.maxime@gmail.com>
2011-02-07 20:22:46 +0100

    Add title to PVCO.

--- src/main/presentation/pvco.cpp
+++ src/main/presentation/pvco.cpp
@@ -1,12 +1,18 @@
 #include "pvco.h"
 
+#include <QFont>
 #include <QGraphicsScene>
+#include <QGraphicsSimpleTextItem>
 #include <QSlider>
 
 PVCO::PVCO()
 {
-    // Resize PVCO to integrate the QSlider nicely.
+    // Resize PVCO to integrate the Components nicely.
     setRect(0, 0, 200, 150);
+
+    // Create a Title.
+    QGraphicsSimpleTextItem* title = new QGraphicsSimpleTextItem("VCO", this);
+    title->setFont(QFont("Courier", 18, QFont::Bold));
 
     // Create a QSlider.
     QSlider* waveSelector = new QSlider(Qt::Horizontal);
@@ -16,7 +22,7 @@ PVCO::PVCO()
     // Add it to the scene using a QGraphicsProxyWidget.
     m_waveSelector = new QGraphicsProxyWidget(this);
     m_waveSelector->setWidget(waveSelector);
-    // Static positioning, that ain't nice.
+    // Static positioning… that ain't nice.
     m_waveSelector->setPos(1, 120);
     m_waveSelector->setMaximumWidth(199);
 }
