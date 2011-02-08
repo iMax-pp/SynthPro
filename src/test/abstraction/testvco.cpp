745dcebad7397a13faee5d36914630287c4fb98c
Cyrille Folliot <archibald.chase@gmail.com>
2011-02-08 08:54:54 +0100

    change name of accessor methods of dimmer value in VCO

--- src/test/abstraction/testvco.cpp
+++ src/test/abstraction/testvco.cpp
@@ -35,7 +35,7 @@ void TestVCO::testVCOwithDimmer()
 
     SimpleFactory factory;
     VCO* vco = factory.createVCO();
-    vco->setDimmerValue(3);
+    vco->setK(3);
     MockSerializerWell output(stream);
     vco->outports().first()->connectTo(&output.input);
     vco->setWaveGenerator(new WaveGeneratorSquare);
