4bc0a9796238e55ae89253cef44db26b61761b1a
Maxime Simon <simon.maxime@gmail.com>
2011-02-09 11:35:11 +0100

    Fix tests compilation.

--- src/test/abstraction/testoutport.cpp
+++ src/test/abstraction/testoutport.cpp
@@ -8,15 +8,3 @@ void TestOutPort::testOut()
     QVERIFY(outPort.out());
     QVERIFY(outPort.buffer()->length() == 900);
 }
-
-// FIXME this test case seems a bit weak
-void TestOutPort::testSwapBuffers()
-{
-    OutPort out(0, "out");
-
-    out.buffer()->data()[0] = 1;
-
-    out.swapBuffers();
-
-    QCOMPARE(out.buffer()->data()[0], 0.0);
-}
