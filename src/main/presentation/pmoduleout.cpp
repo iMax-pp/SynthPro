2e09065a8c9cf35e39fac36bb06b6cbcd0d8d598
Maxime Simon <simon.maxime@gmail.com>
2011-02-10 16:05:56 +0100

    Use Layout on ModuleOutput.

--- src/main/presentation/pmoduleout.cpp
+++ src/main/presentation/pmoduleout.cpp
@@ -2,19 +2,22 @@
 
 #include "control/cmoduleout.h"
 #include "presentation/pport.h"
+#include "presentation/textwidget.h"
+
 #include <QFont>
+#include <QGraphicsAnchorLayout>
 
 PModuleOut::PModuleOut(CModuleOut* control)
     : PModule(control)
 {
 }
 
-void PModuleOut::initialize(PPort* pIn)
+void PModuleOut::initialize(PPort* in)
 {
-    QGraphicsWidget::setGeometry(0, 0, 100, 100);
-
-    QGraphicsSimpleTextItem* title = new QGraphicsSimpleTextItem("Out", this);
+    TextWidget* title = new TextWidget("Out", this);
     title->setFont(QFont("Courier", 18, QFont::Bold));
 
-    pIn->setPos(-7, 45);
+    // Layout
+    leftArea()->addAnchors(in, leftArea());
+    centerArea()->addAnchors(title, centerArea());
 }
