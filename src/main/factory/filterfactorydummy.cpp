5a81338b6fa4c593d88f8644d1e33c6a6ed7d266
Julien Nevo <contact@julien-nevo.com>
2011-02-10 10:26:15 +0100

    Tested VCF, made FilterFactoryDummy, tweaked FilterIncrement.

--- src/main/factory/filterfactorydummy.cpp
+++ src/main/factory/filterfactorydummy.cpp
@@ -1,17 +1,29 @@
 #include "filterfactory.h"
 
 #include "abstraction/filter.h"
+#include "abstraction/filterdummy.h"
 #include "abstraction/filterhp229.h"
+#include "abstraction/filterincrement.h"
 #include "abstraction/filterlp229.h"
 
 FilterFactory::FilterFactory()
 {
+    m_selectorConversionMap.insert(0, "FilterDummy");
+    m_selectorConversionMap.insert(1, "FilterIncrement");
     m_selectorConversionMap.insert(2, "FilterLP229");
     m_selectorConversionMap.insert(3, "FilterHP229");
 }
 
 Filter* FilterFactory::createFilter(const QString& filterType)
 {
+    if (filterType == "FilterDummy") {
+        return new FilterDummy();
+    }
+
+    if (filterType == "FilterIncrement") {
+        return new FilterIncrement();
+    }
+
     if (filterType == "FilterLP229") {
         return new FilterLP229();
     }
@@ -20,10 +32,11 @@ Filter* FilterFactory::createFilter(const QString& filterType)
         return new FilterHP229();
     }
 
-    return new FilterLP229();
+    return new FilterDummy();
 }
 
 QHash<int, QString>& FilterFactory::selectorConversionMap()
 {
     return m_selectorConversionMap;
 }
+
