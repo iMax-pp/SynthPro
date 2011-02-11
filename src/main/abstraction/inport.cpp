3ee8ce88f35af6b3ba3b1c23b23a77b23ffd730d
Julien Nevo <contact@julien-nevo.com>
2011-02-11 21:08:19 +0100

    Inport::fetch optimized.

--- src/main/abstraction/inport.cpp
+++ src/main/abstraction/inport.cpp
@@ -3,6 +3,8 @@
 #include "abstraction/buffer.h"
 #include "abstraction/port.h"
 
+#include <QDebug>
+
 InPort::InPort(Module* parent, const QString& name, SynthProFactory* factory, bool replicable, bool gate)
     : VirtualPort(parent, name, factory, replicable, gate)
 {
@@ -10,6 +12,8 @@ InPort::InPort(Module* parent, const QString& name, SynthProFactory* factory, bo
 
 void InPort::fetch()
 {
+    // Old code.
+    /*
     m_buffer.clear();
 
     for (int i = 0; i < m_buffer.length(); i++) {
@@ -19,4 +23,42 @@ void InPort::fetch()
             }
         }
     }
+    */
+
+    // Optimised code.
+    // We count only the connections that are actually connected to something !
+    int nbConnectionTotal = connections().size();
+    int nbConnectionUsed = 0;
+    for (int i = 0; i < nbConnectionTotal; i++) {
+        if (connections().at(i)->connection()) {
+            nbConnectionUsed++;
+        }
+    }
+
+    // If nothing is connected, clear the buffer.
+    if (nbConnectionUsed == 0) {
+        m_buffer.clear();
+    } else {
+        int size = m_buffer.length();
+        // Scan all the connections to find those connected. The first one is simply duplicated.
+        for (int noPass = 0; noPass < nbConnectionUsed; noPass++) {
+            int noConnexion = 0;
+            if (connections().at(noConnexion)) {
+                // Found a connexion that is used.
+                qreal* dataOut = m_buffer.data();
+                qreal* dataIn = connections().at(noConnexion)->connection()->vPort()->buffer()->data();
+                if (noPass == 0) {
+                    // First connexion found : we duplicate it.
+                    memcpy(dataOut, dataIn, sizeof(qreal) * m_buffer.length());
+                } else {
+                    // The other connexions are added.
+                    for (int i = 0; i < size; i++) {
+                        dataOut[i] += dataIn[i];
+                    }
+                }
+                noConnexion++;
+            }
+        }
+    }
+
 }
