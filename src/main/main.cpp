8730e626ac92f886079f90b55b741b2c5313a220
Julien Richard-Foy <julien@richard-foy.fr>
2011-02-14 08:55:04 +0100

    Build a complex schema at startup

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,3 @@
-#include "abstraction/module/vco.h"
-#include "abstraction/synthpro.h"
-#include "abstraction/wavegen/wavegeneratordummy.h"
 #include "factory/qtfactory.h"
 
 #include <QtGui>
@@ -8,13 +5,33 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    SynthProFactory* factory = new QtFactory();
-    SynthPro* synthpro = factory->createSynthPro();
+    QtFactory* factory = new QtFactory();
+    CSynthPro* synthpro = factory->createSynthPro();
 
-    VCO* vco = factory->createVCO(synthpro);
+    CVCO* vco = factory->createVCO(synthpro);
+    vco->presentation()->setPos(-300, 0);
     synthpro->add(vco);
 
-    vco->process();
+    CVCA* vca = factory->createVCA(synthpro);
+    vca->presentation()->setPos(0, 0);
+    synthpro->add(vca);
+
+    CADSR* adsr = factory->createADSR(synthpro);
+    adsr->presentation()->setPos(0, 200);
+    synthpro->add(adsr);
+
+    CDelay* delay = factory->createDelay(synthpro);
+    delay->presentation()->setPos(250, 0);
+    synthpro->add(delay);
+
+    COscilloscope* oscillo = factory->createOscilloscope(synthpro);
+    oscillo->presentation()->setPos(450, 0);
+    synthpro->add(oscillo);
+
+    vco->outports().first()->connect(vca->inports().first()); // VCO -> VCA
+    adsr->outports().first()->connect(vca->inports().last()); // ADSR -> VCA
+    vca->outports().first()->connect(delay->inports().first()); // VCA -> DELAY
+    delay->outports().first()->connect(oscillo->inports().first()); // DELAY -> OSCILLO
 
     int result = app.exec();
 
