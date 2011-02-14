82ac4ba90a553c723fb480038c525a70f7927b51
Julien Nevo <contact@julien-nevo.com>
2011-02-14 11:36:04 +0100

    Fixed factories.

--- src/main/control/csynthpro.cpp
+++ src/main/control/csynthpro.cpp
@@ -114,7 +114,7 @@ void CSynthPro::addModule(SynthProFactory::ModuleType moduleType, const QPointF&
     }
 
     if (module) {
-        // add(module); // Now the factory does it.
+        add(module);
         dynamic_cast<CModule*>(module)->presentation()->setPos(pos);
     }
 }
