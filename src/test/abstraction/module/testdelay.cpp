8ddf17232a51eedc8afe078c3b14cac0b57702ed
Cyrille Folliot <archibald.chase@gmail.com>
2011-02-14 08:35:43 +0100

    modified:   src/test/abstraction/module/testdelay.cpp

--- src/test/abstraction/module/testdelay.cpp
+++ src/test/abstraction/module/testdelay.cpp
@@ -20,12 +20,24 @@ void TestDelay::testDelay()
     VCO* vco = factory.createVCO(synth);
     vco->outports().first()->connections().first()->connect(delay->inports().first()->connections().first());
 
-    vco->setShape("Sinus");
-    for (int i = 0 ; i < 1000 ; i++) {
-        vco->process();
-        delay->process();
+    // vco->setShape("Sinus");
+    // put 1 in the buffer
+    for (int i = 0 ; i < Buffer::DEFAULT_LENGTH ; i++) {
+        vco->outports().first()->buffer()[i] = 0;
     }
-    /// TODO implement test !
+    for (int j ; j < 3000 ; j++) {
+        delay->process();
+        for (int i = 0 ; i < 20 ; i++) {
+            vco->outports().first()->buffer()[i] = 0;
+        }
+
+        delay->process();
+        for (int k = 0 ; k < Buffer::DEFAULT_LENGTH ; k++) {
+            if (delay->outports().first()->buffer()[k] != 0) {
+                qDebug(k);
+            }
+        }
+    }
 
     delete delay;
     delete synth;
