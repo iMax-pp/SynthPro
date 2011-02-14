30d888a98bf2ef547bfb94c6cac92de652c7861a
Cyrille Folliot <archibald.chase@gmail.com>
2011-02-14 09:48:33 +0100

    implements unit test of Delay and add a third echo in Delay

--- src/test/abstraction/module/testdelay.cpp
+++ src/test/abstraction/module/testdelay.cpp
@@ -11,35 +11,37 @@
 
 #include <QDebug>
 
+
 void TestDelay::testDelay()
 {
-
+    qDebug() << "debut du test";
 
     SimpleFactory factory;
     SynthPro* synth = factory.createSynthPro();
     Delay* delay = factory.createDelay(synth);
     VCO* vco = factory.createVCO(synth);
     vco->outports().first()->connect(delay->inports().first());
+    int nbValues = 10;
+    int nbEcho = 0;
 
-    // vco->setShape("Sinus");
     // put 1 in the buffer
-    for (int i = 0 ; i < Buffer::DEFAULT_LENGTH ; i++) {
-        vco->outports().first()->buffer()[i] = 0;
+    for (int i = 0 ; i <  nbValues ; i++) {
+        vco->outports().first()->buffer()->data()[i] = 1;
     }
-    for (int j ; j < 3000 ; j++) {
+    for (int j = 0 ; j < 4500 ; j++) {
         delay->process();
-        for (int i = 0 ; i < 20 ; i++) {
-            vco->outports().first()->buffer()[i] = 0;
+        for (int i = 0 ; i < Buffer::DEFAULT_LENGTH ; i++) {
+            vco->outports().first()->buffer()->data()[i] = 0;
         }
-
-        delay->process();
+        // read the echo
         for (int k = 0 ; k < Buffer::DEFAULT_LENGTH ; k++) {
-            if (delay->outports().first()->buffer()[k] != 0) {
-                qDebug(k);
+            if (delay->outports().first()->buffer()->data()[k] != 0) {
+                nbEcho++;
             }
         }
     }
-
+    qDebug() << nbValues << " " << nbEcho;
+    QVERIFY(nbEcho == nbValues*4);
     delete delay;
     delete synth;
 }
