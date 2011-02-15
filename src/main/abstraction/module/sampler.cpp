5250bae522e1d418800f259579ed2f0f50dc43b5
Cyrille Folliot <archibald.chase@gmail.com>
2011-02-15 17:03:35 +0100

    sampler is  recording  but no playing... to be continued

--- src/main/abstraction/module/sampler.cpp
+++ src/main/abstraction/module/sampler.cpp
@@ -9,10 +9,14 @@
 #include "abstraction/synthpro.h"
 #include "factory/synthprofactory.h"
 
+#include <QDebug>
+
 Sampler::Sampler(SynthPro* synth) 
     : Module(synth)
     , m_bufferIndex(0)
     , m_sampleSize(0)
+    , m_gateState(false)
+    , m_oldGateState(false)
 {
 
 }
@@ -48,36 +52,85 @@ void Sampler::initialize(SynthProFactory * factory)
 
 void Sampler::ownProcess()
 {
-    if (m_state == RECORDING) {
-        if (m_stop->pushed()) {
-            m_state = WAITING;
-        }  else {
-            m_state = RECORDING;
-        }
+    int sampleMaxInByte = SAMPLER_MAX_DURATION*Buffer::DEFAULT_LENGTH;
+    for (int i = 0 ; i < 10 ; ++i){
+        m_gate->buffer()->data()[i] = 0;
     }
-    if (m_state == EMPTY) {
-        if (m_record->pushed()) {
-            m_state = RECORDING;
-            initializeBuffer();
-        }
+    for (int i = 10 ; i < 100 ; ++i){
+        m_gate->buffer()->data()[i] = 10;
     }
-    if (m_state == WAITING) {
-        if (m_play->pushed()) {
-            m_state = PLAYING;
-        }
-        if (m_record->pushed()) {
-            m_state = RECORDING;
-            initializeBuffer();
-        }
+    for (int i = 100 ; i < 300 ; ++i){
+        m_gate->buffer()->data()[i] = 0;
     }
-    if (m_state == PLAYING) {
-        if (m_stop->pushed()) {
-            m_state = WAITING;
-        }
-
+    for (int i = 300 ; i < Buffer::DEFAULT_LENGTH ; ++i){
+        m_gate->buffer()->data()[i] = 10;
     }
 
-}
+    for (int i = 0 ; i < Buffer::DEFAULT_LENGTH ; i++) {
+
+        if (m_gate->buffer()->data()[i] != 0) {
+            m_gateState = true;
+        } else {
+            m_gateState = false;
+        }
+        bool gateUp = m_gateState && !m_oldGateState;
+        qDebug() <<  m_gate->buffer()->data()[i]  << state() << " " << gateUp;
+
+        if (m_state == RECORDING) {
+            if (m_stop->pushed() || gateUp || m_bufferIndex == sampleMaxInByte) {
+                m_state = WAITING;
+                qDebug() << "stop record here ?" <<  m_bufferIndex <<" " << sampleMaxInByte;
+            }  else if (m_bufferIndex < sampleMaxInByte){
+                m_state = RECORDING;
+            }
+        }
+        if (m_state == EMPTY) {
+            if (m_record->pushed() || gateUp) {
+                m_state = RECORDING;
+                initializeBuffer();
+            }
+        }
+        if (m_state == WAITING) {
+            if (m_play->pushed() || gateUp) {
+                m_state = PLAYING;
+            }
+            if (m_record->pushed()) {
+                m_state = RECORDING;
+                initializeBuffer();
+            }
+        }
+        if (m_state == PLAYING) {
+            if (m_stop->pushed() || gateUp) {
+                m_state = WAITING;
+            } else if (m_bufferIndex == m_sampleSize) {
+                // if the buffer is entirely readed, restart the reading at begin of buffer
+                m_bufferIndex = 0;
+            }
+
+
+        }
+
+        switch (m_state) {
+        case WAITING : break;
+        case EMPTY : break;
+        case PLAYING :
+            m_outPort->buffer()->data()[i] = m_buffer->data()[m_bufferIndex * Buffer::DEFAULT_LENGTH + i];
+            m_bufferIndex+=1;
+            break;
+        case RECORDING :
+            m_buffer->data()[Buffer::DEFAULT_LENGTH*m_bufferIndex +i] = m_inPort->buffer()->data()[i];
+            m_bufferIndex++;
+            m_sampleSize++;
+            break;
+        default : break;
+    } // switch
+        m_oldGateState = m_gateState;
+
+
+
+    } // for
+
+} // ownprocess()
 
 void Sampler::initializeBuffer()
 {
--- src/test/abstraction/module/testsampler.cpp
+++ src/test/abstraction/module/testsampler.cpp
@@ -3,6 +3,7 @@
 #include "abstraction/buffer.h"
 #include "abstraction/component/inport.h"
 #include "abstraction/component/outport.h"
+#include "abstraction/module/keyboard.h"
 #include "abstraction/module/vco.h"
 #include "factory/simplefactory.h"
 
@@ -17,13 +18,26 @@ void TestSampler::testSampler()
     VCO* vco = factory.createVCO(synth);
     vco->outports().first()->connect(sampler->inports().first());
 
+
+    // initialization of the gate port
+    for (int i = 0 ; i < 10 ; ++i){
+        sampler->inports().at(1)->buffer()->data()[i] = 10;
+    }
+
+    for (int i = 10 ; i < Buffer::DEFAULT_LENGTH ; ++i){
+        sampler->inports().at(1)->buffer()->data()[i] = 10;
+    }
+    // initialization of the vco outport
     for (int i = 0 ; i < Buffer::DEFAULT_LENGTH ; i++) {
         vco->outports().first()->buffer()->data()[i] = 5;
     }
-
     sampler->process();
+    for (int i = 0 ; i < Buffer::DEFAULT_LENGTH ; i++) {
+        if (i%100){
+   //         qDebug() << "gate : " << sampler->inports().at(1)->buffer()->data()[i];
+        }
+    }
 
-    qDebug() << "state " << sampler->state();
 
 
 }
