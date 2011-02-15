430eee1e09f296b5f1ba7db5463882fc5086da4e
Julien Nevo <contact@julien-nevo.com>
2011-02-15 23:16:22 +0100

    Began stabilisation of the oscillsocope. Some success, but to be improved.

--- src/main/presentation/widget/poscilloscopeview.cpp
+++ src/main/presentation/widget/poscilloscopeview.cpp
@@ -1,11 +1,14 @@
 #include "poscilloscopeview.h"
 
 #include "abstraction/module/vco.h"
+
+#include <QDebug>
 #include <QPainter>
 
 POscilloscopeView::POscilloscopeView(QGraphicsItem* parent)
     : QGraphicsWidget(parent)
     , m_ratioY(RATIO_Y_DEFAULT)
+    , m_stabilized(true)
 {
     setMinimumSize(boundingRect().size());
 #if QT_VERSION >= 0x040700
@@ -22,20 +25,48 @@ void POscilloscopeView::paint(QPainter* painter, const QStyleOptionGraphicsItem*
 
     int currentRatioY = ((HEIGHT / 2) / VCO::SIGNAL_INTENSITY) * m_ratioY;
 
-    int indexBuffer = 0;
     int middleY = HEIGHT / 2;
     int previousY = middleY;
+    int usedBufferSize = m_inBuffer->length() - BUFFER_INVISIBLE;
 
     if (m_inBuffer) {
         qreal* data = m_inBuffer->data();
-        qreal step = (m_inBuffer->length() / WIDTH);
+        qreal step;
+        int indexBuffer = 0;
+
+        if (m_stabilized) {
+            // Find the first high-shelf in a specified interval.
+            bool found = false;
+            qreal previousValue = 32000;
+            qreal currentValue;
+            int size = m_inBuffer->length() / 2; // We only test a part of the buffer.
+
+            while (!found && (indexBuffer < size)) {
+                currentValue = data[indexBuffer];
+                if (previousValue < currentValue) {
+                    found = true;
+                    // qDebug() << "FOUND !" << indexBuffer << "  Pre = " << previousValue << "  Current = " << currentValue;
+                } else {
+                    indexBuffer++;
+                }
+                previousValue = currentValue;
+                indexBuffer++;
+
+            }
+            if (!found) {
+                indexBuffer = 0;
+            }
+        }
+
+        step = ((usedBufferSize - indexBuffer) / WIDTH);
 
         for (int i = 0; i < WIDTH ; i++) {
-            int y = (int)(middleY + data[(int)indexBuffer] * currentRatioY);
+            int y = (int)(middleY - data[(int)indexBuffer] * currentRatioY);
             painter->drawLine(i - 1, previousY, i, y);
             previousY = y;
             indexBuffer += step;
         }
+
     }
 }
 
