8fb00e498dcc3a2935ce4450c494e56b721a1260
Julien Nevo <contact@julien-nevo.com>
2011-02-16 11:51:59 +0100

    Added the stabilize button.

--- src/main/abstraction/module/oscilloscope.cpp
+++ src/main/abstraction/module/oscilloscope.cpp
@@ -1,9 +1,11 @@
 #include "oscilloscope.h"
 
+#include "abstraction/component/pushbutton.h"
 #include "factory/synthprofactory.h"
 
 Oscilloscope::Oscilloscope(SynthPro* parent)
     : Module(parent)
+    , m_stabilized(false)
 {
 }
 
@@ -17,14 +19,27 @@ void Oscilloscope::initialize(SynthProFactory* factory)
         // Creation of an Input.
         m_inPort = factory->createInPortReplicable(this, "in");
         m_inports.append(m_inPort);
+
+        m_stabilizeControl = factory->createPushButton("Stabilize", this);
+
+        connect(m_stabilizeControl, SIGNAL(buttonPushed()), this, SLOT(stabilizedPushed()));
     }
 }
 
 void Oscilloscope::timerExpired()
 {
-
 }
 
 void Oscilloscope::ownProcess()
 {
+}
+
+bool Oscilloscope::stabilized()
+{
+    return m_stabilized;
+}
+
+void Oscilloscope::stabilizedPushed()
+{
+    m_stabilized = !m_stabilized;
 }
