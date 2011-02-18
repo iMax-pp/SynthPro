817643adacd4eefdb148d36b77da13abb1341dea
Maxime Simon <simon.maxime@gmail.com>
2011-02-18 10:59:29 +0100

    Add doxygen comments on CSynthPro and ModuleListModel.

--- src/main/control/csynthpro.h
+++ src/main/control/csynthpro.h
@@ -10,6 +10,9 @@
 class CVirtualPort;
 class QGraphicsScene;
 
+/**
+ * SynthPro control class (PAC by inheritance).
+ */
 class CSynthPro : public SynthPro {
     Q_OBJECT
 
@@ -17,18 +20,35 @@ public:
     CSynthPro(SynthProFactory*);
     virtual ~CSynthPro();
 
+    /// @returns the associated PSynthPro presentation.
     inline PSynthPro* presentation() const { return m_presentation; }
+
+    /// @param presentation of type PSynthPro to associate with this CSynthPro.
     void setPresentation(PSynthPro*);
 
+    /// Override SynthPro::add to add the presentation when Module is successfully added.
     void add(Module*);
+
+    /// Create a new Module with the given ModuleType and at the given QPointF.
     void addModule(SynthProFactory::ModuleType, const QPointF&);
 
+    /**
+     * Show drag&drop feedback from given CVirtualPort to every single port.
+     * @see hideFeedback
+     */
     void showFeedback(CVirtualPort*);
+
+    /**
+     * Hide drag&drop feedback from given CVirtualPort to every single port.
+     * @see showFeedback
+     */
     void hideFeedback();
 
+    /// Create a new scheme (delete every module).
     void newScheme();
 
 private slots:
+    /// Slot called when Play/Pause button is clicked.
     void play(bool);
 
 private:
--- src/main/control/modulelistmodel.h
+++ src/main/control/modulelistmodel.h
@@ -5,18 +5,34 @@
 #include <QAbstractListModel>
 #include <QStringList>
 
+/**
+ * This class holds three lists for the different Module:
+ *    -# Their names;
+ *    -# Their pixmaps;
+ *    -# Their ModuleType.
+ *
+ * It is intended to be used in the Docks on the presentation to display the Module.
+ */
 class ModuleListModel : public QAbstractListModel {
 public:
     ModuleListModel(QObject* parent = 0);
 
+    /// Overrides QAbstractListModel::rowCount.
     int rowCount(const QModelIndex& parent = QModelIndex()) const;
+
+    /// Overrides QAbstractListModel::flags.
     Qt::ItemFlags flags(const QModelIndex&) const;
 
+    /// Overrides QAbstractListModel::data.
     QVariant data(const QModelIndex&, int = Qt::DisplayRole)const;
 
+    /// Overrides QAbstractListModel::mimeData.
     QMimeData* mimeData(const QModelIndexList&) const;
+
+    /// Overrides QAbstractListModel::mimeTypes.
     QStringList mimeTypes() const;
 
+    /// Add a Module to the lists, by its name and ModuleType.
     void addModule(const QString& moduleName, const QtFactory::ModuleType);
 
 private:
