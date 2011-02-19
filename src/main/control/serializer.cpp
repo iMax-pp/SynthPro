8287d5cfd43a5d83933c19f425c767c5a07096f7
Maxime Simon <simon.maxime@gmail.com>
2011-02-19 21:54:50 +0100

    fixup! Add save button in toolbar; Serialize Module Name and Position.

---
+++ src/main/control/serializer.cpp
@@ -0,0 +1,24 @@
+#include "serializer.h"
+
+#include "abstraction/module.h"
+#include "control/cmodule.h"
+#include "control/csynthpro.h"
+
+void operator<<(QTextStream& stream, const CSynthPro& synthPro)
+{
+    stream << synthPro.modules().size() << endl;
+
+    foreach (Module* module, synthPro.modules()) {
+        CModule* cmodule = dynamic_cast<CModule*>(module);
+        stream << *cmodule;
+        stream << endl;
+    }
+}
+
+void operator<<(QTextStream& stream, const CModule& module)
+{
+     // Serialize Module real type.
+    stream << typeid(module).name() << " ";
+    // Serialize Module position.
+    stream << module.presentation()->pos().rx() << " " << module.presentation()->pos().ry();
+}
