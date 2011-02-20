8f49d9624ed575c44af3be0e1bf5388bedba80e7
Julien Nevo <contact@julien-nevo.com>
2011-02-20 14:04:26 +0100

    Little refactoring of timers of Clock, added comments.

--- src/main/abstraction/module/speaker.cpp
+++ src/main/abstraction/module/speaker.cpp
@@ -27,7 +27,7 @@ Speaker::~Speaker()
 {
     // Unregister the module as a fast timer to the Clock.
     Clock& clock = Clock::instance();
-    clock.unregister(this);
+    clock.unregisterFastTimer(this);
 
     // Release the device.
     AudioDeviceProvider& adp = AudioDeviceProvider::instance();
@@ -53,12 +53,12 @@ void Speaker::initialize(SynthProFactory* factory)
 
         // Register to a fast timer to the Clock.
         Clock& clock = Clock::instance();
-        clock.registerFastClock(this);
+        clock.registerFastTimer(this);
     }
 }
 
 
-void Speaker::timerExpired()
+void Speaker::fastTimerExpired()
 {
     int fillCounter = 0;
     qint64 sizeWritten = 1;
--- src/main/abstraction/module/timecriticalmodule.h
+++ src/main/abstraction/module/timecriticalmodule.h
@@ -3,13 +3,19 @@
 
 #include "abstraction/module.h"
 
+/**
+ * Module used when needed time critical accuracy, like the Speaker.
+ * Basically, the Clock can register these modules using fast timers,
+ * and will call the timerExpired slot when it is expired.
+ */
 class TimeCriticalModule : public virtual Module {
     Q_OBJECT
+
 public:
     TimeCriticalModule(SynthPro*);
 
 public slots:
-    virtual void timerExpired() = 0;
+    virtual void fastTimerExpired() = 0;
 };
 
 #endif // TIMECRITICALMODULE_H
